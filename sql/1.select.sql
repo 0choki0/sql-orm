69dfb23916e7fd4ab8b47c1e17923940f3fa57e0
jiho <19wlgh95@naver.com>
2023-11-15 14:46:03 +0900

    sql의 기본적인 문법

---
+++ sql/1.select.sql
@@ -0,0 +1,11 @@
+-- python manage.py shell_plus --ipython
+
+-- Movie.objects.all()
+-- SELECT * FROM movies_movie;
+
+-- User.objects.all()
+-- SELECT * FROM movies_user;
+
+-- Movie.objects.all().order_by('-year')
+-- SELECT * FROM movies_movie
+-- ORDER BY year DESC;
---
+++ sql/4.delete.sql
@@ -0,0 +1,7 @@
+-- -- actor = Actor.objects.get(id=1)
+-- -- actor.delete()
+-- DELETE FROM movies_actor
+-- WHERE id=1;
+
+-- SELECT * FROM movies_actor
+-- WHERE id=1;
